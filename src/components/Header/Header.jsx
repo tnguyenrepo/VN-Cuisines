f5390dd11083c9c52e656f1c40027096cc8e21a0
Tai Nguyen <nguyenxuantai1986@gmail.com>
2025-03-05 10:29:10 +0000

    Edit Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
       )}
       <div className="headerContent">
         <img src={Logo} alt="logo" />
-        <h3>Discover the best food & drinks in Patna</h3>
+        <h3>Discover the best food & drinks in India</h3>
         <div className="input">
           <select name="" id="">
             <option value="Chennai">Chennai</option>
